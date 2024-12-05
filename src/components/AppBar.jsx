d4617460f90c1308416aa6f564908dab2fa66b2e
Vitalii-250885 <provitaleks@gmail.com>
2024-12-05 10:23:24 +0200

    correct abbBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -72,22 +72,22 @@ function AppBar() {
   return (
     <div
       ref={AppBarRef}
-      className="animation-timeline bg-buttonGroup top-10 z-30 hidden animate-emergence items-center gap-2.5 rounded-full backdrop-blur-3xl sm:absolute sm:top-[52vw] sm:flex sm:p-[0.53vw] lg:top-[48vw] lg:p-[0.32vw] 2xl:top-[49vw] 2xl:p-[0.175vw]"
+      className="animation-timeline top-10 z-30 hidden animate-emergence items-center gap-2.5 rounded-full bg-buttonGroup backdrop-blur-3xl sm:absolute sm:top-[52vw] sm:flex sm:p-[0.53vw] lg:top-[48vw] lg:p-[0.32vw] 2xl:top-[49vw] 2xl:p-[0.175vw]"
     >
       <a
-        className="bg-buttonHover flex items-center justify-center rounded-full sm:h-[4.95vw] sm:w-[6vw] lg:h-[4.43vw] lg:w-[6.1vw] 2xl:h-[2.53vw] 2xl:w-[3.3vw]"
+        className="bg-appBarBtn flex items-center justify-center rounded-full sm:h-[4.95vw] sm:w-[6vw] lg:h-[4.43vw] lg:w-[6.1vw] 2xl:h-[2.53vw] 2xl:w-[3.3vw]"
         href="#"
       >
         <img
           src="img/logo_dark.svg"
           alt="logo HisCoder"
-          className="sm:w-[3.01vw] lg:w-[3.43vw] 2xl:w-[1.83vw]"
+          className="fill-white sm:w-[3.01vw] lg:w-[3.43vw] 2xl:w-[1.83vw]"
         />
       </a>
       {appBarButtons.map(({ label, href }, index) => (
         <a
           key={index}
-          className="text-basic last:bg-buttonHover active:bg-buttonHover lg:hover:bg-buttonHover flex items-center justify-center whitespace-nowrap rounded-full bg-transparent font-bold duration-150 active:duration-150 sm:h-[4.95vw] sm:px-[1.86vw] sm:text-[1.42vw] lg:h-[4.43vw] lg:px-[1.6vw] lg:text-[1.1vw] lg:hover:duration-150 lg:active:bg-transparent 2xl:h-[2.53vw] 2xl:px-[0.88vw] 2xl:text-[0.61vw]"
+          className="last:bg-appBarBtn active:bg-appBarBtn lg:hover:bg-appBarBtn flex items-center justify-center whitespace-nowrap rounded-full bg-transparent font-bold text-white duration-150 active:duration-150 sm:h-[4.95vw] sm:px-[1.86vw] sm:text-[1.42vw] lg:h-[4.43vw] lg:px-[1.6vw] lg:text-[1.1vw] lg:hover:duration-150 lg:active:bg-transparent 2xl:h-[2.53vw] 2xl:px-[0.88vw] 2xl:text-[0.61vw]"
           href={href}
         >
           {label}
